406f9013349f19b56ce2a306490329469a5dbda9
ArtemPopof <Deviling42@gmail.com>
2020-12-11 23:21:58 +0300

    admin panel and coding code task screen

--- src/js/screens/admin.js
+++ src/js/screens/admin.js
@@ -1,17 +1,85 @@
 import React from 'react';
 import MainHeader from '../components/Header';
 import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
 import * as Api from '../services/ApiController';
+import cookie from 'js-cookie';
 import axios from 'axios';
 
 export default class AdminScreen extends React.Component {
 
     render() {
         return (
-            <div>
-                ADMIN PANEL
+        <div>
+                <MainHeader/>
+                <div className="content">
+                    <h1 style={{marginTop: "40px", marginBottom: "40px"}} className="label_header center-hor">LearnIT Admin panel</h1>
+                    
+                    <h3 className="label_header center-hor">Add new question</h3>
+                    <div className="col-md-8 lcard center-hor-content center-hor">
+                        <h2>Question</h2>
+                        <InputText id="question"></InputText>
+                        <h2>Answers</h2>
+                        <InputText id="answers"></InputText>
+                        <h2>Correct</h2>
+                        <InputText id="correct"></InputText>
+                        <h2>Level</h2>
+                        <InputText id="level"></InputText>
+                        <br/>
+                        <Button style={{marginTop: "40px"}} onClick={() => this.saveQuestion()} label="Save" className="button p-button-warning"/>
+                    </div>
+
+                    <h3 style={{marginTop: "55px"}} className="label_header center-hor">Add new code task</h3>
+                    <div className="col-md-8 lcard center-hor-content center-hor">
+                        <h2>Title</h2>
+                        <InputText id="title"></InputText>
+                        <h2>Summary</h2>
+                        <InputText id="summary"></InputText>
+                        <h2>Description</h2>
+                        <InputText id="description"></InputText>
+                        <h2>Areas</h2>
+                        <InputText id="areas"></InputText>
+                        <h2>Level</h2>
+                        <InputText id="task_level"></InputText>
+                        <br/>
+                        <Button style={{marginTop: "40px"}} onClick={() => this.saveTask()} label="Save" className="button p-button-warning"/>
+                    </div>
+                </div>
             </div>
         )
     }
+
+    saveTask() {
+        var titleValue = document.getElementById('title').value
+        var summaryValue = document.getElementById('summary').value
+        var descriptionValue = document.getElementById('description').value
+        var areasValue = document.getElementById('areas').value
+        var levelValue = document.getElementById('task_level').value
+
+        axios({
+            method: 'post',
+            url: Api.API_URL + '/code/save',
+              headers: {
+                'Authorization': "Bearer " + cookie.get('token'),
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+              },
+            data:JSON.stringify({
+                title: titleValue,
+                shortDescription: summaryValue,
+                fullDescription: descriptionValue,
+                areas: areasValue,
+                level: levelValue
+            })
+        }).then(response => {
+            console.log("saved task")
+            console.log(response.data)
+            document.location.href("/admin")
+        }
+        ).catch(response => {
+            console.log("error")
+            console.log(response)
+        });  
+      }
 }
--- src/js/screens/code.js
+++ src/js/screens/code.js
@@ -3,9 +3,31 @@ import MainHeader from '../components/Header';
 import { Button } from 'primereact/button';
 
 import * as Api from '../services/ApiController';
+import cookie from 'js-cookie';
 import axios from 'axios';
 
 export default class CodeTaskScreen extends React.Component {
+
+    componentDidMount() {
+        console.log("loading questions")
+        axios({
+            method: 'get',
+            url: Api.API_URL + '/code/availableTasks?level=' + this.getLevel(),
+            headers: {
+                'Authorization': "Bearer " + cookie.get('token'),
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }).then(response => {
+            console.log("received tasks")
+            console.log(response.data)
+            this.setState({tasks: response.data})
+        }
+        ).catch(error => {
+            console.log("error")
+            console.log(error)
+        });     
+     }
 
     render() {
         return (
@@ -15,15 +37,7 @@ export default class CodeTaskScreen extends React.Component {
                     <div className="center-hor-content" style={{paddingTop: "50px"}}>
                         <h1 className="label_header" style={{marginBottom: "50px", marginTop: "50px"}}>Выберете задание уровня {this.getLevel()}</h1>
                         <div className="row">
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
+                            {this.renderAllTasks()}
                             <div className="col-md-3" style={{marginTop: "25px"}}>
                                 <div className="clickable-card lncard" style={{margin: "0"}}>
                                     <h3>Эхо</h3>
@@ -101,6 +115,10 @@ export default class CodeTaskScreen extends React.Component {
                 </div>
             </div>
         )
+    }
+
+    renderAllTasks() {
+
     }
 
     getLevel() {
