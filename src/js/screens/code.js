d60d34e2964652cc664c2ea081ebc341558e723c
ArtemPopof <Deviling42@gmail.com>
2020-12-12 21:06:32 +0300

    code page improvements

--- src/js/screens/code.js
+++ src/js/screens/code.js
@@ -8,6 +8,10 @@ import axios from 'axios';
 
 export default class CodeTaskScreen extends React.Component {
 
+    state = {
+
+    }
+
     componentDidMount() {
         console.log("loading questions")
         axios({
@@ -26,6 +30,7 @@ export default class CodeTaskScreen extends React.Component {
         ).catch(error => {
             console.log("error")
             console.log(error)
+            this.setState({taskLoadingError: error})
         });     
      }
 
@@ -38,78 +43,6 @@ export default class CodeTaskScreen extends React.Component {
                         <h1 className="label_header" style={{marginBottom: "50px", marginTop: "50px"}}>Выберете задание уровня {this.getLevel()}</h1>
                         <div className="row">
                             {this.renderAllTasks()}
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
-                            <div className="col-md-3" style={{marginTop: "25px"}}>
-                                <div className="clickable-card lncard" style={{margin: "0"}}>
-                                    <h3>Эхо</h3>
-                                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>Ввод с клавиатуры и печать текста обратно пользователю</p>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Java Core</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Console</span>
-                                    <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>Code</span>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -118,7 +51,52 @@ export default class CodeTaskScreen extends React.Component {
     }
 
     renderAllTasks() {
+        if (this.state.taskLoadingError) return this.showError()
+        if (this.state.tasks == null || this.state.tasks.size == 0) return this.emptyListWarning(); 
 
+        return this.state.tasks.map((task) => this.renderTask(task))
+
+    }
+
+    renderTask(task) {
+        var areas = task.areas.split(';')
+        
+        return (
+            <div className="col-md-3" style={{marginTop: "25px"}}>
+                <div className="clickable-card lncard" style={{margin: "0"}}>
+                    <h3>{task.title}</h3>
+                    <p style={{fontSize: "18px", fontWeight: "200", color: "rgba(255, 248, 248, 0.486)"}}>{task.shortDescription}</p>
+                    {areas.map((area) => this.renderArea(area))}
+                </div>
+            </div>
+        )
+    }
+
+
+    renderArea(area) {
+        return (
+           <span className="p-tag p-tag-rounded" style={{marginRight: "20px"}}>{area}</span>
+        )
+    }
+
+    emptyListWarning() {
+        return (
+            <div className="col-md-12" style={{marginTop: "25px"}}>
+                <div className="lncard" style={{margin: "0"}}>
+                    <h1>Нет доступных заданий</h1>
+                </div>
+            </div>
+        )
+    }
+
+    showError() {
+        return (
+            <div className="col-md-12" style={{marginTop: "25px"}}>
+                <div className="lncard" style={{margin: "0"}}>
+                    <h1>Ошибка при загрузке заданий, повторите попытку позже</h1>
+                </div>
+            </div>
+        )
     }
 
     getLevel() {
